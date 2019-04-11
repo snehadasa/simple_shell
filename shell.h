65ccf3885e3ef8ec7ddb5f7996ea0567bfd7344c
vdphan <673@holbertonschool.com>
2019-04-11 20:51:56 +0000

    update header file

--- shell.h
+++ shell.h
@@ -7,9 +7,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <string.h>
 
 void _puts(char *str);
 int _putchar(char c);
 int _strcmp(char *s1, char *s2);
+char **handle(char *buffer);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_getenv(const char *name);
 
 #endif /* SHELL_H */
