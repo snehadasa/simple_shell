2f8067ee8e9fb034b9bb9273777f6be945faef9c
vdphan <673@holbertonschool.com>
2019-04-17 06:30:50 +0000

    update comment

--- built_in.c
+++ built_in.c
@@ -3,11 +3,23 @@
  *built_in - create built-in command for shell.
  *@tokenize: the value after tokenizing the buffer in main file.
  *@env: array of string of environment values.
+ *@buff: buffer input from main.
  */
-void built_in(char *tokenize, char **env)
+void built_in(char **tokenize, char **env, char **buff)
 {
-	if (_strcmp(tokenize, "exit") == 0)
+	int i;
+	(void)buff;
+
+	if (_strcmp(tokenize[0], "exit") == 0)
+	{
+		for (i = 0; tokenize[i]; i++)
+			free(tokenize[i]);
+		free(*buff);
+		free(tokenize);
 		exit(0);
-	if (_strcmp(tokenize, "env") == 0)
+	}
+	if (_strcmp(tokenize[0], "env") == 0)
+	{
 		env_builtin(env);
+	}
 }
--- shell.h
+++ shell.h
@@ -10,19 +10,37 @@
 #include <string.h>
 
 extern char **environ;
-
+/* create puts and putchar */
 void _puts(char *str);
 int _putchar(char c);
+
+/* handle_arg.c */
+char *_strdup(char *str);
+char **handle(char *buffer);
+
+/* path.c*/
+char *get_path_value(char **env);
+char *path_check(char *token, char **env);
+char *_strstr(char *haystack, char *needle);
+
+/*strhelp.c*/
 int _strcmp(char *s1, char *s2);
 int _strncmp(const char * s1, const char * s2, size_t n);
-char *get_path_value(char **env);
-char **handle(char *buffer);
 char *_strcat(char *dest, char *src);
 int _strlen(char *str);
 char *_memset(char *s, char b, unsigned int n);
-char *path_check(char *token, char **env);
+
+/*built_in.c*/
+void built_in(char **tokenize, char **env, char **buff);
+
+/*please_enter.c*/
+char *readtheline(void);
+void safefree(char *str);
+void dfree(char **str);
+
+/*simple_shell_main.c*/
 void env_builtin(char **env);
-void built_in(char *tokenize, char **env);
-char *readline(void);
+void free2(char **tokenize, char *buff);
+void free3(char **tokenize, char *buff);
 
 #endif /* SHELL_H */
